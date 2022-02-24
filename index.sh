d760c04c95bd1549d0f3fcd9f43861b927011988
kueikao <r08921109@ntu.edu.tw>
2022-02-24 19:34:30 +0800

    fix: rename tello driver

--- index.sh
+++ index.sh
@@ -10,7 +10,7 @@ source /home/$USER/Documents/shell/ros.sh
 
 command () {
     echo "(1) ccd:    cd to the path"
-    echo "(2) tello:  start tello driver"
+    echo "(2) driver:  start tello driver"
     echo "(3) it:     start image transport node"
     echo "(4) pyv:    switch python version"
 }
